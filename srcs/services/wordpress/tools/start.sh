47d0a9542eefd67775e729e5d66feef72e3875cf
PythonGermany <97847597+PythonGermany@users.noreply.github.com>
2023-08-13 07:33:11 +0200

    Back to old config setup

--- srcs/services/wordpress/tools/start.sh
+++ srcs/services/wordpress/tools/start.sh
@@ -3,12 +3,12 @@ for DOMAIN in $DOMAINS; do
   if ! wp core is-installed --path=/var/www/$DOMAIN --allow-root; then
     echo "Installing and setting up wordpress for $DOMAIN..."
     # Set up wordpress config file
-    DB_NAME="wp_$DOMAIN"
-    DB_USER="user_$DOMAIN"
-    DB_PASSWORD=abahhAWCNAWFUIBAwufgaoiHFUWfiahwgf
-    wp config create --path=/var/www/$DOMAIN --allow-root \
-      --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD \
-      --dbhost=$MYSQL_HOST
+    cp wp-config-template.php wp-config.php
+    sed -i "s/DB_NAME/$DB_NAME/g" wp-config.php
+    sed -i "s/DB_USER/$DB_USER/g" wp-config.php
+    sed -i "s/DB_PASSWORD/$DB_PASSWORD/g" wp-config.php
+    sed -i "s/DB_HOST/$MYSQL_HOST/g" wp-config.php
+    mv wp-config.php /var/www/$DOMAIN
     # Install wordpress site
     wp core install --path=/var/www/$DOMAIN --allow-root \
     --url="https://$DOMAIN" --title="placeholder" --admin_user="lol" \

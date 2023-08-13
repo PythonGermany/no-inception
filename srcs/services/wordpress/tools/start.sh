38a50cfddb1e14da325d1132a0db1cf5d256185a
PythonGermany <97847597+PythonGermany@users.noreply.github.com>
2023-08-13 06:47:46 +0200

    Changed wordpress start order

--- srcs/services/wordpress/tools/start.sh
+++ srcs/services/wordpress/tools/start.sh
@@ -2,17 +2,17 @@ set -e
 for DOMAIN in $DOMAINS; do
   if ! wp core is-installed --path=/var/www/$DOMAIN --allow-root; then
     echo "Installing and setting up wordpress for $DOMAIN..."
-    # Set up wordpress config files
+    # Install wordpress site
+    wp core install --path=/var/www/$DOMAIN --allow-root \
+    --url="https://$DOMAIN" --title="placeholder" --admin_user="lol" \
+    --admin_password="abahhAWCNAWFUIBAwufgaoiHFUWfiahwgf" --admin_email=none@none.com
+    # Set up wordpress config file
     DB_NAME="wp_$DOMAIN"
     DB_USER="user_$DOMAIN"
     DB_PASSWORD=abahhAWCNAWFUIBAwufgaoiHFUWfiahwgf
     wp config create --path=/var/www/$DOMAIN --allow-root \
       --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD \
       --dbhost=$MYSQL_HOST
-    # Install wordpress site
-    wp core install --path=/var/www/$DOMAIN --allow-root \
-    --url="https://$DOMAIN" --title="placeholder" --admin_user="lol" \
-    --admin_password="abahhAWCNAWFUIBAwufgaoiHFUWfiahwgf" --admin_email=none@none.com
     # Set up files permissions for wordpress
     chown -R www-data:www-data /var/www/$DOMAIN
     find /var/www/$DOMAIN -type d -exec chmod 755 {} \;
